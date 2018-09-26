917cd9b3f11dde81207d17883f20c64512a6d594
Glenn Cariga <glenncariga@gmail.com>
2018-09-26 10:01:58 -0500

    final

--- src/WorstPerformingStock.java
+++ src/WorstPerformingStock.java
@@ -1,23 +1,24 @@
 class WorstPerformingStock{
-    public static void main(String[] args) {
-         public final int[][] stocks={{1,12,24},{2,34,56},{3,65,32},{4,103,0}};
-    
-        findStock(stocks);
-    }
 
-    public static void findStock(stocks){
-        int id;
-        int lowest;
+    public static int findStock(int[][] stocks){
+        int id=0;
+        int lowest=0;
         int performance;
         for(int i =0; i<stocks.length;i++){
             for(int j = 0; j<stocks[i].length;j++){
-                performance = (stock[i][1]-stock[i][2])/stock[i][1];
+                performance = (stocks[i][1]-stocks[i][2])/stocks[i][1];
                 if (lowest > performance){
                     lowest = performance;
-                    id = stock[i][0]
+                    id = stocks[i][0];
                 }
             }
         }
-        System.out.println("Worst Performing is: "+id);
+       return id;
     }
+    public static void main(String[] args) {
+        final int[][] stocks={{1,12,24},{2,34,56},{3,65,32},{4,103,0}};
+    
+        System.out.println("Worst Performing is: "+findStock(stocks));
+    }
+
 }
