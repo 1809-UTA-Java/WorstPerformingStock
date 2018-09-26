497acacfbda604483d9123c520f191e06e4514df
Zach Leonard <zachariah.s.leonard@gmail.com>
2018-09-26 10:24:29 -0500

    Reformatting to meet design requirements

--- src/com/revature/WorstPerformingStock.java
+++ src/com/revature/WorstPerformingStock.java
@@ -6,21 +6,25 @@ package com.revature;
 */
 class WorstPerformingStock {
 	public static void main (String[] args) {
-		float[][] stockValues;
+		int[][] stockValues;
+		
+		//id number, an opening price, and a closing price
+		stockValues = new int[][] { {42, 4635, 5640}, {44, 342, 345}, {45, 2345, 2124}, {99, 1, 5000}};
+		System.out.print("Worst performing Stock ID: ");
+		System.out.print(WorstPerformingStock(stockValues) + "\n");
+	}
+	public static int WorstPerformingStock(int[][] stockValues) {
 		int lowestIndex = 0;
 		float lowestStockValue = 0f;
 		float tempStockValue = 0f;
-		//id number, an opening price, and a closing price
-		stockValues = new float[][] { {42f, 4635f, 5640f}, {44f, 342f, 345f}, {45f, 2345f, 2124f}, {99f, 1f, 5000f}};
 		for(int i = 0; i < stockValues.length; i++) {
-		//worst performing stock following the equation: (OpeningPrice - ClosingPrice) / OpeningPrice
-			tempStockValue = (stockValues[i][1] - stockValues[i][2]) / stockValues[i][1];
+		//worst performing stock following the equation: ( ClosingPrice - OpeningPrice) / OpeningPrice
+			tempStockValue = (stockValues[i][2] - stockValues[i][1]) / (float) stockValues[i][1];
 			if (tempStockValue < lowestStockValue) {
 				lowestStockValue = tempStockValue;
 				lowestIndex = i;
 			}
 		}
-		System.out.print("Worst performing Stock ID: ");
-		System.out.print(stockValues[ lowestIndex][0] + "\n");
+		return stockValues[lowestIndex][0];
 	}
 }
